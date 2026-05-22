2512699abc4f115017648e1fe5f3a3de27e0c162
cheeseburgerguy <130573430+cheeseburgerguy@users.noreply.github.com>
2026-05-22 06:08:38 -0500

    Update config.js

--- public/config.js
+++ public/config.js
@@ -1,1 +1,10 @@
-let _CONFIG = {};
+let _CONFIG = {
+    prefix: '/service/',
+    bare: '/bare/',
+    encodeUrl: Scramjet.codec.xor.encode,
+    decodeUrl: Scramjet.codec.xor.decode,
+    search: 'https://www.google.com/search?q=%s'
+};
+
+// Map it to self.__scramjet_config just in case the Service Worker looks for it there
+self.__scramjet_config = _CONFIG;
